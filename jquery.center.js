4772c05aef2b2d5735b338b8faf0f8795a420fd3
Mustafah <me@mustafah.me>
2014-01-11 23:28:13 +0200

    Initial upload

---
+++ jquery.center.js
@@ -0,0 +1,23 @@
+$.fn.center = function (options) {
+	options = options || {}
+	options.this = options.this || {}
+	options.inside = options.inside || {}
+
+	if (options.this.h == null) options.this.h = 0.5
+	if (options.this.v == null) options.this.v = 0.5
+
+	options.inside.el = $(options.inside.el || this.parent())
+	if (options.inside.h == null) options.inside.h = 0.5
+	if (options.inside.v == null) options.inside.v = 0.5
+
+	if (options.hOffset == null) options.hOffset = 0;
+	if (options.vOffset == null) options.vOffset = 0;
+
+	this.css("position","absolute");
+
+	this.css("left", (options.inside.el.width() * options.inside.h - this.width() * options.this.h + options.hOffset) + "px");
+
+	this.css("top", (options.inside.el.height() * options.inside.v - this.height() * options.this.v + options.vOffset)  + "px");
+
+	return this;
+}
